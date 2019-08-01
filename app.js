2d72d163a32b0c0c352b8ff8d20278707c8f555d
Lance Russell <lancebryanrussell@gmail.com>
2019-08-01 12:42:41 -0500

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const movies = require('./movies.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
+
+function handleGetMovies(req, res){
+    let response = movies
+
+    if(req.query.genre){
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+          )
+    }
+
+    if(req.query.country){
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+          )
+    }
+
+    if(req.query.avg_vote){
+        response = response.filter(movie =>
+            movie.avg_vote >= Number(req.query.avg_vote)
+        )
+    }
+
+    res.json(response);
+}
+
+app.get('/movies', handleGetMovies)
+
+module.exports = app;
+
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+
+app.listen(8000, () => {
+    console.log(`Server listening at http://localhost:8000`)
+})
